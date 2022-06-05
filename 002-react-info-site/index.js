f7e2f8f2f2e6aaacff4a972245fb5c9fc756acbe
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-05 14:32:03 -0600

    update

--- 002-react-info-site/index.js
+++ 002-react-info-site/index.js
@@ -1,8 +1,33 @@
 //ReactDOM.render(<h1> Hello Yong !</h1>, document.getElementById("root"));
+
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+function Navbar() {
+    return (
+        <div class="navbar">
+            <div class="navbar-inner">
+                <a class="brand" href="#">Title</a>
+                <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                </ul>
+            </div>
+         </div>
+    )
+}
 ReactDOM.render(
-    <ul>
-        <li>Thing 1</li>
-        <li>Thing 2</li>
-    </ul>, 
+    <div>
+        <ul>
+            <li>Thing 1</li>
+            <li>Thing 2</li>
+        </ul>
+        <Navbar />
+        <MainContent />
+    </div>,
     document.getElementById("root")
 );

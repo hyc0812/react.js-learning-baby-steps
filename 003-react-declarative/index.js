bf2586a12b92603137be2bc612b762686caa8e4a
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-05 14:44:17 -0600

    update

--- 003-react-declarative/index.js
+++ 003-react-declarative/index.js
@@ -1,2 +1,16 @@
 //ReactDOM.render(<h1> Hello Yong !</h1>, document.getElementById("root"));
-ReactDOM.render(<h1> Hello Yong !</h1>, document.getElementById("root"));
+/*
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div #root
+*/
+const h1_1 = document.createElement("h1");
+h1_1.textContent = "This is an imperative way to program!";
+//h1.className = "header"
+
+const h1_2 = document.createElement("h1");
+h1_2.textContent = "This is the second h1 element";
+document.getElementById("root").append(h1_1);
+document.getElementById("root").append(h1_2);
+

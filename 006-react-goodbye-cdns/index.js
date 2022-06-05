9f35724a0a729f5bff759d06580780789fbf1e42
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-05 17:17:49 -0600

    update

---
+++ 006-react-goodbye-cdns/index.js
@@ -0,0 +1,45 @@
+import React from "react"
+import ReactDOM from "react-dom"
+const navbar  = (
+    <nav>
+        <h1> Yongchang's website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar, document.getElementById("root")
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const navbar = (
+//     <nav>
+//         <h1> Yong's Website</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contract</li>
+//         </ul>
+//     </nav>
+// )
+// ReactDOM.render(
+//     navbar, document.getElementById("root")
+// );
